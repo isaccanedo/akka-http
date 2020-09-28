802f9c2eb40a0450fe1b21b1a9860d6d8288d755
isaccanedo <isaccanedo@gmail.com>
2020-09-28 12:31:09 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/akka/http/UserActor.java
@@ -0,0 +1,41 @@
+package br.com.isaccanedo.akka.http;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.japi.pf.FI;
+import br.com.isaccanedo.akka.http.UserMessages.ActionPerformed;
+import br.com.isaccanedo.akka.http.UserMessages.CreateUserMessage;
+import br.com.isaccanedo.akka.http.UserMessages.GetUserMessage;
+
+
+class UserActor extends AbstractActor {
+
+  private UserService userService = new UserService();
+
+  static Props props() {
+    return Props.create(UserActor.class);
+  }
+
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+            .match(CreateUserMessage.class, handleCreateUser())
+            .match(GetUserMessage.class, handleGetUser())
+            .build();
+  }
+
+  private FI.UnitApply<CreateUserMessage> handleCreateUser() {
+    return createUserMessageMessage -> {
+      userService.createUser(createUserMessageMessage.getUser());
+      sender().tell(new ActionPerformed(String.format("User %s created.", createUserMessageMessage.getUser()
+              .getName())), getSelf());
+    };
+  }
+
+  private FI.UnitApply<GetUserMessage> handleGetUser() {
+    return getUserMessageMessage -> {
+      sender().tell(userService.getUser(getUserMessageMessage.getUserId()), getSelf());
+    };
+  }
+
+}
